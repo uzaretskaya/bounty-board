260cc48f9d50d9d1a47285941b6e0eba713eaaa6
zaretskaiaud <mail@zaretskaya.ru>
2025-07-21 23:07:47 +0300

    chapters 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,16 +7,10 @@ fun main() {
     println("$HERO_NAME announces her presence to the world.")
     println("What level is $HERO_NAME?")
 
-    val playerLevelInput = readLine()!!
-    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
-        playerLevelInput.toInt()
-    } else {
-        1
-    }
+    playerLevel = readlnOrNull()?.toIntOrNull() ?: 0
     println("$HERO_NAME's level is $playerLevel.")
 
     readBountyBoard()
-
 
     println("Time passes...")
     println("$HERO_NAME returns from her quest.")
@@ -27,12 +21,19 @@ fun main() {
 }
 
 private fun readBountyBoard() {
-    println(
-        """
-        $HERO_NAME approaches the bounty board. It reads:
-           "${obtainQuest(playerLevel).replace("Nogartse", "xxxxxxxxx")}"
-    """.trimIndent()
-    )
+    val message = try {
+        obtainQuest(playerLevel)?.replace("Nogartse", "xxxxxxxxx")?.let {
+            println(
+                """
+                $HERO_NAME approaches the bounty board. It reads:
+                    "$it"
+                """.trimIndent()
+            )
+        } ?: "$HERO_NAME approaches the bounty board, but it is blank."
+    } catch (e: Exception) {
+        "$HERO_NAME can't read what's on the bounty board."
+    }
+    println(message)
 }
 
 private fun obtainQuest(
@@ -40,19 +41,28 @@ private fun obtainQuest(
     playerClass: String = "paladin",
     hasBefriendedBarbarians: Boolean = true,
     hasAngeredBarbarians: Boolean = false
-) = when (playerLevel) {
-    1 -> "Meet Mr. Bubbles in the land of soft things."
-    in 2..5 -> {
-        val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
-
-        if (canTalkToBarbarians)
-            "Convince the barbarians to call off their invasion."
-        else
-            "Save the town from the barbarian invasions."
+): String? {
+    if (playerLevel <= 0) {
+        throw InvalidPlayerLevelException()
     }
 
-    6 -> "Locate the enchanted sword."
-    7 -> "Recover the long-lost artifact of creation."
-    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-    else -> "There are no quests right now."
+    return when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+
+            if (canTalkToBarbarians)
+                "Convince the barbarians to call off their invasion."
+            else
+                "Save the town from the barbarian invasions."
+        }
+
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        //else -> "There are no quests right now."
+        else -> null
+    }
 }
+
+class InvalidPlayerLevelException() : IllegalArgumentException("Player level must be at least 1.")
